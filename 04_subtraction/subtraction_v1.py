bd0e644606ab85b1530634b34d9a2056a6f83044
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-05-30 23:45:01 +0000

    Attempt to get subtraction v1 to work

--- 04_subtraction/subtraction_v1.py
+++ 04_subtraction/subtraction_v1.py
@@ -12,18 +12,22 @@ def random_generator(num_1, num_2):
 
 
 
-def subtraction():
+def subtraction(want_neg):
  answer = 0
- if want_neg_int == "Yes":
-   while: 
+   while answer == 0: 
+    num_1 = random_generator(min_num, max_num)
+    num_2 = random_generator(min_num, max_num)
+    answer = num_1 + num_2
+   while answer == 0 or answer < 0: 
     num_1 = random_generator(min_num, max_num)
     num_2 = random_generator(min_num, max_num)
     answer = num_1 + num_2
 
+
  user_answer = "False"
  while user_answer == "False":
   try:
-   user_answer = int(input("{} + {}? ".format(num_1, num_2)))
+   user_answer = int(input("{} - {}? ".format(num_1, num_2)))
   except:
     print("This is not a number! ")
    
@@ -38,11 +42,10 @@ def subtraction():
 
 score_list = []
 
-want_neg_int = input("Do you want integers")
-for item in range (5):
-  min_num = 1
-  max_num = 10
-  answer_check = addition()
-  score_list.append(answer_check)
+want_neg_int = input("Do you want integers ")
+min_num = 1
+max_num = 10
+answer_check = subtraction(want_neg_int)
+score_list.append(answer_check)
 
 print(score_list)
